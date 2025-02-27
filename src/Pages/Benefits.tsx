aa9f20dd9b25b0ee287344a345ab3516681ba0f6
joshua <jozzydgreat1@gmail.com>
2025-02-27 02:21:30 -0800

    added gem images

--- src/Pages/Benefits.tsx
+++ src/Pages/Benefits.tsx
@@ -1,4 +1,4 @@
-import Powder from '../assets/powdermineral.jpg'
+import rufiles from '../assets/rufiles-mineral.jpg'
 export const Benefit = ()=>{
     return(
         <section>
@@ -6,20 +6,22 @@ export const Benefit = ()=>{
                 <h3 className='animate-left'>Why Choose Us?</h3>
                 <div className="row">
                     <div className="col-md-6">
+                        <p className='animate-right'>
                         We are certified professionals dedicated to providing high-quality organic minerals to
                         industries worldwide. Through our strong global supply network and strategic partnerships,
                         We ensure the seamless export of premium-grade minerals that meet international standards.
                         Our commitment to excellence guarantees reliable and sustainable solutions for all your mineral needs.
+                        </p>
                         <div>
-                            <button className='btn btn-success '>Contact Us</button>
+                            <button className='btn btn-success animate-up '>Contact Us</button>
                             {/* <ion-icon name="heart"></ion-icon> */}
 
                         </div>
                     </div>
-                    <div className="col-md-6">
-                        {/* <div>
-                        <img src={Powder} alt='organic material' style={{width:'30%', border:'solid'}}/>
-                        </div> */}
+                    <div className="col-md-6" style={{ display:'flex', justifyContent:'center'}}>
+                        
+                        
+                        
                     </div>
                 </div>
             </div>
--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -5,20 +5,20 @@ export const Product = ()=>{
                 <div className="product-grid row animate-left">
                     <div className=' col-md-4'>
                         
-                        <div className="products">
-                        <div className='proList'>Minerals</div>
+                        <div className="product-limonite">
+                        <div className='proList'>Lithium</div>
                         </div>
                     </div>
                     <div className=' col-md-4'>
                         
-                        <div className="products">
-                        <div className='proList'>Minerals</div>
+                        <div className="product-mica">
+                        <div className='proList'>Mica</div>
                         </div>
                     </div>
                     <div className=' col-md-4'>
                         
-                        <div className="products">
-                        <div className='proList'>Minerals</div>
+                        <div className="product-rufite">
+                        <div className='proList'>Rufite</div>
                         </div>
                     </div>
                     
